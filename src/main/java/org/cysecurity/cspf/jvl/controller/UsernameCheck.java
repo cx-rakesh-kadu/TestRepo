ef00f6265fc9df0abe6223270d435b563b735df7
Rakesh Kadu <141014001+cx-rakesh-kadu@users.noreply.github.com>
2026-07-16 14:35:04 +0530

    Checkmarx Bot - Auto Pull Request

--- src/main/java/org/cysecurity/cspf/jvl/controller/UsernameCheck.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/UsernameCheck.java
@@ -9,8 +9,8 @@ package org.cysecurity.cspf.jvl.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -48,8 +48,10 @@ public class UsernameCheck extends HttpServlet {
                 if(con!=null && !con.isClosed())
                 {
                     ResultSet rs=null;
-                    Statement stmt = con.createStatement();  
-                    rs=stmt.executeQuery("select * from users where username='"+user+"'");
+                    // Use PreparedStatement to prevent SQL injection
+                    PreparedStatement stmt = con.prepareStatement("select * from users where username=?");
+                    stmt.setString(1, user);
+                    rs=stmt.executeQuery();
                     if (rs.next()) 
                     {  
                      json.put("available", "1"); 
